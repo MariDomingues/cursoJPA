96ff0e710c089fb72db395f361c5d0eaab4be49f
Mariane <ma_ap_domingues@hotmail.com>
2021-12-05 15:56:11 -0300

    Inicio da aplicação

---
+++ src/main/java/model/Produto.java
@@ -0,0 +1,48 @@
+package model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "produto")
+public class Produto {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private int id;
+//    @Column(name = "nome")
+    private String nome;
+    private String descricao;
+    private double preco;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+}
